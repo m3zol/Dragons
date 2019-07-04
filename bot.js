be37647afa93ce3fde047fc52f769d46af23d02f
m3zol <41552322+m3zol@users.noreply.github.com>
2019-07-05 00:43:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,6 +56,17 @@ message.channel.send("`Error`:" + Julian)
   });
 
 //
+client.on('message', async message => {
+  let args = message.content.slice(3);
+  if(message.content.startsWith('D*bc')) {
+    if(!message.guild.members.get(message.author.id).hasPermission('ADMINISTRATOR')) return message.channel.send('Required Administrator Permission')
+       message.guild.members.forEach(m => {
+      
+      m.send(args.replace('[user]', m).replace('[server]', m.guild.name).replace('[sender]', message.author.username))
+    })
+  }
+})
+
 
 
 
