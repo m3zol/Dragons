8748bf9111efe8a8e43a332d388a131e2748575b
m3zol <41552322+m3zol@users.noreply.github.com>
2019-07-05 02:05:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -106,45 +106,6 @@ client.on("guildMemberAdd", member => {
 })
 //
 
-Majd.on('message', message => { //clear
-    if(!message.channel.guild) return;
- if(message.content.startsWith('D*clear')) {
- if(!message.channel.guild) return message.channel.send('**هذا الامر فقط للسيرفرات**').then(m => m.delete(5000));
- if(!message.member.hasPermission('MANAGE_MESSAGE')) return      message.channel.send('**ليس لديك برمشن manage message`' );
- let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
- let request = `Requested By ${message.author.username}`;
- message.channel.send(`**هل انت متأكد من حذف الشات؟**`).then(msg => {
- msg.react('✅')
- .then(() => msg.react('❌'))
- .then(() =>msg.react('✅'))
-
- let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
- let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-
- let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
- let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
- reaction1.on("collect", r => {
- message.channel.send(`سينحذف الشات ...`).then(m => m.delete(5000));
- var msg;
-         msg = parseInt();
-
-       message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-       message.channel.sendMessage("", {embed: {
-         title: "`` تــــم حذف الشات ``",
-         color: 0x06DF00,
-         footer: {
-
-         }
-       }}).then(msg => {msg.delete(3000)});
-
- })
- reaction2.on("collect", r => {
- message.channel.send(`**تم الغاء حذف الشات**`).then(m => m.delete(5000));
- msg.delete();
- })
- })
- }
- });
 
 
 
