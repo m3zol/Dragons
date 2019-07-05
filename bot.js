0d2b87d3f36c4cf59fd7d6dfe6a217cb1818b0d0
m3zol <41552322+m3zol@users.noreply.github.com>
2019-07-05 02:10:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -105,6 +105,13 @@ client.on("guildMemberAdd", member => {
 }).catch(console.error)
 })
 //
+client.on("message", message => {
+  if (message.content.startsWith('D*send')) {
+    if(!message.author.id === "595701769401663556") return;
+    var user = message.mentions.members.first();
+    var args = message.content.split(" ").slice(1).join(" ");
+user.send(args);
+  }});
 
 
 
