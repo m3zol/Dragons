2216f2e8d3155ebc84b6393fc9dfb8c0bae1d82e
m3zol <41552322+m3zol@users.noreply.github.com>
2019-10-01 09:58:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -169,6 +169,32 @@ if (message.content.startsWith('D*members')) { // BY KIllerFox ==== KillerFox
     })
     }
 });
+//
+
+client.on("message", message => {
+   
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+                if(!message.channel.guild) return;
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**No Permissions**');
+        var msg;
+        msg = parseInt();
+     
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done",
+        color: 0x36393e,
+        description: "The Room chat has been Deleted !",
+        footer: {
+          text: "DRG Bot©."
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+ 
+     
+});
+
 
 
 
