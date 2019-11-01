2ac3af321634e8424cf74b1375b2393758fb4d6e
m3zol <41552322+m3zol@users.noreply.github.com>
2019-11-01 22:38:46 +0200

    Update boot.js

--- boot.js
+++ boot.js
@@ -1,1 +1,15 @@
+const Discord = require('discord.js')
+const { Client, Util } = require('discord.js');
+const queue = new Map();
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
 
