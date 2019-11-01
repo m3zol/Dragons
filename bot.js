d33790e24b3a7176a1dc271a0f61631156e7514b
m3zol <41552322+m3zol@users.noreply.github.com>
2019-11-01 18:32:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -149,26 +149,29 @@ let text = args.replace(room, "");
 
 });
 //
-bot.on('messageCreate', async(message) => {
-  if(message.author.bot || !message.channel.guild) return undefined;
-  let args = message.content.toLowerCase().split(' ');
-  let cmd = args[0].slice(prefix.length);
-  if(cmd == `avatar`){
-    let avt =  message.mentions[0] ? message.member.guild.members.get(message.mentions[0].id) : message.member.guild.members.find(m => m.user.id == args[1] || m.user.username.toLowerCase().includes(args[1]) || m.user.discriminator == args[1]) || message.member;
-    let avtName;
-    if(avt == message.member) {avtName = `Your`} else {avtName = `${avt.user.username}'s`};
-    if(args[1] == 'server'){
-      if(message.content.toLowerCase().endsWith('-url')) return bot.createMessage(message.channel.id, message.member.guild.iconURL || 'No Icon.');
-      await bot.createMessage(message.channel.id, {embed: {image: { url: message.member.guild.iconURL || 'https://bitsofco.de/content/images/2018/12/broken-1.png' }, footer: { text: bot.user.username, icon_url: bot.user.avatarURL }, author: { name: 'Server Icon', icon_url: message.author.avatarURL }}});
-    } else {
-      if(message.content.toLowerCase().endsWith('-url')) return bot.createMessage(message.channel.id, avt.user.avatarURL || 'No Avatar.');
-      await bot.createMessage(message.channel.id, {embed: {image: { url: avt.user.avatarURL || 'https://bitsofco.de/content/images/2018/12/broken-1.png'}, footer: { text: bot.user.username, icon_url: bot.user.avatarURL }, author: { name: `${avtName} Avatar`, icon_url: message.author.avatarURL }}});
-  }
+client.on("message", message => {
+if(message.content.startsWith("avatar")){
+if(message.author.bot || message.channel.type == "dm") return;
+var avtmnt = message.mentions.users.first();
+var avt = avtmnt || message.author;
+var args = message.content.split(" ")[1];
+if(args == "server"){
+let avts = new Discord.RichEmbed()
+.setColor("#36393e")
+.setAuthor(`Server Avatar`, message.author.avatarURL)
+.setImage(message.guild.iconURL)
+.setFooter(`PrimeBot.`, message.client.user.avatarURL);
+message.channel.send(avts);
+} else {
+let avtm = new Discord.RichEmbed()
+.setColor("#36393e")
+.setAuthor(`${avt.username}, message.author.avatarURL)
+.setImage(avt.avatarURL)
+.setFooter(`PrimeBot.`, message.client.user.avatarURL);
+message.channel.send(avtm);
+}
 }
 });
-   
-
-
 
 
 
